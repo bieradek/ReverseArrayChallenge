99c124b829f56b7b2d417ec618036330851c0ec7
Radosław Biedrzycki <radoslaw.biedrzycki@syncron.com>
2018-09-19 15:44:36 +0200

    Uploading working solution

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -5,19 +5,33 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        int[] array = {4, 1, 5, 2, 12}; // --> [12, 2, 5, 1, 4]
-        System.out.println("Normal array: " + Arrays.toString(array));
+        int[] array = {5,2,212,19,8,6,4, 1, 5, 2, 12}; // --> [12, 2, 5, 1, 4]
         reverse(array);
     }
 
     public static void reverse(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            array[i] = array[array.length - i]; // 4 --> 4
+        System.out.println("Array: " + Arrays.toString(array));
+        int[] arrayCopy = new int[array.length];
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            arrayCopy[i] = array[i];
+            counter++;
         }
-        System.out.println("Reversed array: " + Arrays.toString(array));
+        counter--;
+        System.out.println("Array copy before reverse: " + Arrays.toString(arrayCopy));
+        System.out.println("Counter equal to:" + counter);
+        int[] reversedArray = new int[array.length];
+        for (int i = 0; i <= counter; i++) {
+            reversedArray[i] = arrayCopy[counter - i];
+//            0 -> 4
+//            1 -> 3
+//            2 -> 2
+//            3 -> 1
+//            4 -> 0
+        }
+        System.out.println("***********************************");
+        System.out.println("Array: " + Arrays.toString(array));
+        System.out.println("Array copy: " + Arrays.toString(arrayCopy));
+        System.out.println("Reversed array: " + Arrays.toString(reversedArray));
     }
 }
-
-
-//
