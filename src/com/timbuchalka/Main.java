ead352f554eefcf8ef5a6f5fa06a4ac8dcf08d74
Radosław Biedrzycki <radoslaw.biedrzycki@syncron.com>
2018-09-19 15:45:25 +0200

    Uploading final code

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {5,2,212,19,8,6,4, 1, 5, 2, 12}; // --> [12, 2, 5, 1, 4]
+        int[] array = {5, 2, 212, 19, 8, 6, 4, 1, 5, 2, 12}; // --> [12, 2, 5, 1, 4]
         reverse(array);
     }
 
@@ -23,15 +23,6 @@ public class Main {
         int[] reversedArray = new int[array.length];
         for (int i = 0; i <= counter; i++) {
             reversedArray[i] = arrayCopy[counter - i];
-//            0 -> 4
-//            1 -> 3
-//            2 -> 2
-//            3 -> 1
-//            4 -> 0
         }
-        System.out.println("***********************************");
-        System.out.println("Array: " + Arrays.toString(array));
-        System.out.println("Array copy: " + Arrays.toString(arrayCopy));
-        System.out.println("Reversed array: " + Arrays.toString(reversedArray));
     }
 }
